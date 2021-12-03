ec4bb9ab62be60cdcaf39c8a3df3fef7feed0485
Mohammad Imran Khan <imranbutex41@gmail.com>
2021-12-03 15:56:45 +0600

    final

--- src/pages/Home/Projects/CompleteProject/CompleteProject.js
+++ src/pages/Home/Projects/CompleteProject/CompleteProject.js
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// const CompleteProject = () => {
-//     return (
-//         <div className="vh-100">
-//             <h2>Complete Projects are coming very soon.....</h2>
-//         </div>
-//     );
-// };
+const CompleteProject = () => {
+    return (
+        <div className="vh-100">
+            <h2>Complete Projects are coming very soon.....</h2>
+        </div>
+    );
+};
 
-// export default CompleteProject;
+export default CompleteProject;
--- src/pages/Home/Projects/JavascriptProjects/JavascriptProjects.js
+++ src/pages/Home/Projects/JavascriptProjects/JavascriptProjects.js
@@ -1,42 +1,42 @@
-// import React from 'react';
-// import { Carousel,Button } from 'react-bootstrap';
-// import mackbook from "../../../../images/javascript/macbook-bootstrap-javascript.png"
-// const JavascriptProjects = () => {
-//     return (
-//         <div>
-//             <div className="row ">
-//                 <div className="col-lg-7 col-md-6 col-sm-12 col-12 scrollable">
+import React from 'react';
+import { Carousel,Button } from 'react-bootstrap';
+import mackbook from "../../../../images/javascript/macbook-bootstrap-javascript.png"
+const JavascriptProjects = () => {
+    return (
+        <div>
+            <div className="row ">
+                <div className="col-lg-7 col-md-6 col-sm-12 col-12 scrollable">
                 
-//                     <Carousel variant="dark">
-//                     <Carousel.Item>
-//                     <img className="img-fluid" src={mackbook} alt="" />
-//                     </Carousel.Item>
+                    <Carousel variant="dark">
+                    <Carousel.Item>
+                    <img className="img-fluid" src={mackbook} alt="" />
+                    </Carousel.Item>
                    
-//                     </Carousel>
-//                 </div>
-//                 <div className="col-lg-5 col-md-6 col-sm-12 col-12">
-//                 <h2 className="text-danger">Mac Book Pro</h2>
+                    </Carousel>
+                </div>
+                <div className="col-lg-5 col-md-6 col-sm-12 col-12">
+                <h2 className="text-danger">Mac Book Pro</h2>
                
-//                 <div className="text-start ">
-//                     <p ><i  className="fas fa-plus"></i>Developed a website where you will feel the E-commerce site like. It is a single page design   </p> 
-//                     <p><i  className="fas fa-plus"></i>Here Mainly javascript is used to make a functional landing page website </p>
-//                 </div>
+                <div className="text-start ">
+                    <p ><i  className="fas fa-plus"></i>Developed a website where you will feel the E-commerce site like. It is a single page design   </p> 
+                    <p><i  className="fas fa-plus"></i>Here Mainly javascript is used to make a functional landing page website </p>
+                </div>
                 
              
-//                 <p className="text-start "><strong >Technologies:</strong> 
-//                 <Button variant="outline-danger m-1">JavaScript</Button>
-//                 <Button variant="outline-danger m-1">Bootstrap</Button>
-//                 <Button variant="outline-danger m-1">Netlify</Button>
-//                 </p>
-//                 <p className="text-start "><strong >All Links:</strong> 
-//                 <Button variant="outline-danger m-1">  <a className="  text-decoration-none" href="https://github.com/imrankhan41/mac-book-pro" target="_blank" rel="noopener noreferrer">GitHub </a> </Button>
+                <p className="text-start "><strong >Technologies:</strong> 
+                <Button variant="outline-danger m-1">JavaScript</Button>
+                <Button variant="outline-danger m-1">Bootstrap</Button>
+                <Button variant="outline-danger m-1">Netlify</Button>
+                </p>
+                <p className="text-start "><strong >All Links:</strong> 
+                <Button variant="outline-danger m-1">  <a className="  text-decoration-none" href="https://github.com/imrankhan41/mac-book-pro" target="_blank" rel="noopener noreferrer">GitHub </a> </Button>
              
-//                 <Button variant="outline-danger m-1">  <a className="text-decoration-none" href="https://macbook-bootstrap-javascript-website.netlify.app/" target="_blank" rel="noopener noreferrer">Live Preview</a> </Button>
-//                </p>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+                <Button variant="outline-danger m-1">  <a className="text-decoration-none" href="https://macbook-bootstrap-javascript-website.netlify.app/" target="_blank" rel="noopener noreferrer">Live Preview</a> </Button>
+               </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default JavascriptProjects;
+export default JavascriptProjects;
